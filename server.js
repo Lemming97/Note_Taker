53f084e8d37f14e66eddbc0430a75734a16c4bf2
Lindsey Martin <martinlindsey87@yahoo.com>
2022-06-15 23:49:05 -0700

    added routes

--- server.js
+++ server.js
@@ -17,21 +17,9 @@ app.use(express.static('public'));
 
 
 // Use apiRoutes
-app.use('/api/notes', (req, res) => {
-    res.json(allNotes.slice(1));
-});
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
